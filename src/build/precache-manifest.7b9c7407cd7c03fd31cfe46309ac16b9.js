c1799e5ef39ad2251849c418e82161d2d5ffa474
tarek.saleh <tarek.saleh@amdocs.com>
2022-09-02 13:01:09 +0300

    First Commit

---
+++ src/build/precache-manifest.7b9c7407cd7c03fd31cfe46309ac16b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1983ac640eb0929d919",
+    "url": "/static/css/main.1686c654.chunk.css"
+  },
+  {
+    "revision": "d1983ac640eb0929d919",
+    "url": "/static/js/main.a8cd8b3a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f7125d3c337e9d104bda",
+    "url": "/static/js/2.f8b8cd58.chunk.js"
+  },
+  {
+    "revision": "bc1ebc21dc040ceb6d24d4817497dcbe",
+    "url": "/index.html"
+  }
+];
